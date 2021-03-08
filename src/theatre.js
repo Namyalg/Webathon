8242da028b3b8389bf5c38fc2eec52046839732b
Namya LG <53875297+Namyalg@users.noreply.github.com>
2021-03-08 19:47:46 +0530

    Update theatre.js

--- src/theatre.js
+++ src/theatre.js
@@ -15,7 +15,7 @@ function settheatre() {
       if (!firebase.apps.length) {
           firebase.initializeApp(firebaseConfig);
       }else {
-          firebase.app(); // if already initialized, use that one
+          firebase.app();
       }
       var database = firebase.database().ref('Link');
       var link = window.localStorage.getItem("Linktovideo");
@@ -23,46 +23,30 @@ function settheatre() {
       const storage = firebase.storage();
       var n = window.localStorage.getItem("Name");
       var k = window.localStorage.getItem("Key")
-      //var firebaseOrdersCollection = database.ref().child('Questions');
        database.child(link).on('value',(snap)=>{
-         //console.log(link);
-         //console.log("My name is ");
-         //console.log(window.localStorage.getItem("Name"));
-         //console.log(window.localStorage.getItem("Key"));
-    
-         //console.log(snap.val());
          var l = snap.val();
          if(l.charAt(0) == "1"){
             var ytlink = snap.val();
             var h = ytlink.split("=");
             var final = h[h.length - 1];
             ytlink = final;
-             //console.log("Yotube");
              var url = "https://www.youtube.com/embed/" + ytlink +"?autoplay=1";
-             //console.log(url);
              document.getElementById("myFrame").src = url;
          }
          else if(l.charAt(0) == "2"){
             var url = snap.val();
             url = url.split("^");
             url = url[1];
-            //console.log("Drive");
             document.getElementById("myFrame").src = url;
         }
         else{
             var file_url;
-            //console.log("device");
             storage.ref(snap.val()).getDownloadURL()
             .then((url) => {
-              //console.log(url);
-              //console.log("insde");
               file_url = url;
-                 document.getElementById("myFrame").src = file_url;
-              //console.log(file_url);
-                
+                 document.getElementById("myFrame").src = file_url;       
           });
         }
-        //console.log('chat stuff');
         console.log("https://chat-at-remo.herokuapp.com/?key=" + k +"&name=" + n);
         document.getElementById("chatit").src = "https://chat-at-remo.herokuapp.com/?key=" + k +"&name=" + n;
       });
